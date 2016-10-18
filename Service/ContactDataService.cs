20d62482f3c7e3ddb87e264b44166f9c90676cc8
TecPrime Solutions <tecprime@tecprime.net>
2016-10-18 21:48:56 -0200

    Plugin view

--- Service/ContactDataService.cs
+++ Service/ContactDataService.cs
@@ -1,4 +1,5 @@
 ﻿using NopBrasil.Plugin.Widgets.ContactData.Models;
+using System.Text;
 
 namespace NopBrasil.Plugin.Widgets.ContactData.Service
 {
@@ -11,11 +12,36 @@ namespace NopBrasil.Plugin.Widgets.ContactData.Service
             this._contactDataSettings = contactDataSettings;
         }
 
-        public PublicInfoModel GetModel()
+        private void AddText(StringBuilder sb, string text)
         {
-            PublicInfoModel model = new PublicInfoModel();
-            model.Html = "NopCommerce Brasil";
-            return model;
+            if (!string.IsNullOrEmpty(text))
+            {
+                if (sb.Length > 0)
+                {
+                    if (_contactDataSettings.ViewMode.Equals("One line"))
+                        sb.Append(" - ");
+                    else
+                        sb.Append("<br/>");
+                }
+                sb.Append(text);
+            }
         }
+
+        private string GetFormattedString()
+        {
+            StringBuilder sb = new StringBuilder();
+            AddText(sb, _contactDataSettings.CompanyName);
+            AddText(sb, _contactDataSettings.NationalRegisterCorporate);
+            AddText(sb, _contactDataSettings.PhoneNumber);
+            AddText(sb, _contactDataSettings.Email);
+            AddText(sb, _contactDataSettings.Address);
+            AddText(sb, _contactDataSettings.District);
+            AddText(sb, _contactDataSettings.PostalCode);
+            AddText(sb, _contactDataSettings.City);
+            AddText(sb, _contactDataSettings.State);
+            return sb.ToString();            
+        }
+
+        public PublicInfoModel GetModel() => new PublicInfoModel() { Html = GetFormattedString() };
     }
 }
