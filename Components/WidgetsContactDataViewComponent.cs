6dab1cd1747aaf9eba00ffb779235edcf980f811
Romulo AC <rcianci@gmail.com>
2018-07-13 01:19:37 -0300

    Migração para versão 4.00 - Inclusão dos novos arquivos necessários

---
+++ Components/WidgetsContactDataViewComponent.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Framework.Components;
+using NopBrasil.Plugin.Widgets.ContactData.Service;
+
+namespace NopBrasil.Plugin.Widgets.ContactData.Component
+{
+    [ViewComponent(Name = "WidgetsContactData")]
+    public class WidgetsContactDataViewComponent : NopViewComponent
+    {
+        private readonly IContactDataService _contactDataService;
+
+        public WidgetsContactDataViewComponent(IContactDataService contactDataService)
+        {
+            this._contactDataService = contactDataService;
+        }
+
+        public IViewComponentResult Invoke(string widgetZone, object additionalData) => View("~/Plugins/Widgets.ContactData/Views/PublicInfo.cshtml", _contactDataService.GetModel());
+    }
+}
